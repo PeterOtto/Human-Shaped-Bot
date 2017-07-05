4026919481db9a0a2b937efa3acc270cea07a3f8
PeterOK <MUCpeku@Peters-MacBook-Pro.local>
2017-07-05 18:51:15 +0200

    Initial commit. Project set up, and node installed plus first “hello world” tweet send.

---
+++ twitterBot.js
@@ -0,0 +1,10 @@
+console.log("The twitter bot is starting");
+
+var Twit = require('twit')
+var config = require('./config')
+
+var T = new Twit(config);
+
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
