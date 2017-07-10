2fbbef7ce9d24bd807bf9152c06a7f1a79041fac
PeterOK <MUCpeku@Peters-MacBook-Pro.local>
2017-07-10 19:07:03 +0200

    added listener for hashtags parsing location from hashtags
    Need to do
    put it in one function to make it a bit smarter.

--- twitterBot.js
+++ twitterBot.js
@@ -13,14 +13,24 @@ var firebaseConfig = require('./config/firebaseConfig.js');
 var T = new Twit(twitterConfig);
 //setting up a new instance of firebase, called app - for now
 var app = firebase.initializeApp(firebaseConfig);
+// Get a reference to the database service
+var database = firebase.database();
+//setting up access to the google maps API
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBbiaUFvlp2Skd7m4qm6FdBv0611rJbf-o'
+});
+
+/*-------------------------------------------------------------------------------------------
+
+	This section is for handeling and replaying to people tweeting directly @NotHumanShaped
+
+-------------------------------------------------------------------------------------------*/
+//Random greeting to prevent twitter for blocking duplicate tweets
+var greetings = ["Hello","Hi","G’day","Howdy","Hey","Ciao","Aloha","Salut","Hej"];
 // creates a var called stream, where we store data from the twitter stream
 var stream = T.stream("user");
 //when we recieve a tweet on the stream send the data to tweetReceived
-stream.on("tweet", tweetReceived)
-// Get a reference to the database service
-var database = firebase.database();
-//Random greeting to prevent twitter for blocking duplicate tweets
-var greetings = ["Hello","Hi","G’day","Howdy","Hey","Ciao","Aloha","Salut","Hej"];
+stream.on("tweet", tweetReceived);
 
 //function for handeling the data that we recive from the tweet
 function tweetReceived(eventMsg) {
@@ -38,14 +48,13 @@ function tweetReceived(eventMsg) {
 			var place = eventMsg.place.bounding_box.coordinates;
 			var cityName = eventMsg.place.name;
 			var img = eventMsg.entities.media[0].media_url_https;
-			console.log(img);
 			saveDataFromTweet(text, from, place, location, cityName, img);
 			//reply to the person
 			replyToUser(from, "Thanks for sharing that with me, I've add it to our website.");
 		}
 		
 		/*
-		This could be done a lot smarter, with setting some flags, but for now we'll just be doing it with it statements instead haha!
+		This could be done a lot smarter, with setting some flags, but for now we'll just be doing it with if statements instead haha!
 		*/
 
 		//check to see if they have shared their location with the bot
@@ -65,6 +74,43 @@ function tweetReceived(eventMsg) {
 		}
 	}
 }
+
+/*-------------------------------------------------------------------------------
+
+	This section is for handeling tweets with the #nothumanshaped and replaying
+
+--------------------------------------------------------------------------------*/
+
+var stream = T.stream('statuses/filter', { track: 'nothumanshaped,humanshapedworld,' })
+
+stream.on('tweet', hashtagFound);
+function hashtagFound(tweet) {
+	console.log("We got a tweet");
+	for (var i = tweet.entities.hashtags.length - 1; i >= 0; i--) {
+		// Geocode the hashtags.
+		googleMapsClient.geocode({address: tweet.entities.hashtags[i].text},
+		function(err, response) {
+			if (!err && response.json.status === "OK") {
+				console.log("Valid tweet found!")
+		  		validTweet(response,tweet);
+		  	}
+		});
+	}
+}
+
+function validTweet(response,tweet) {
+	console.log("running validTweet function");
+	
+	var from = tweet.user.screen_name;
+	var text = tweet.text;
+	var location = [response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng];
+	var place = "na";
+	var cityName = response.json.results[0].formatted_address;
+	var img = tweet.entities.media[0].media_url_https;
+	saveDataFromTweet(text, from, place, location, cityName, img);
+	console.log("Saveing data");
+}
+
 // Here we push the data to firebase
 // Eventually we want to store multiple tweets from the same user.
 // We also want to store some more info on them, profile pic, link, mail etc for the website
@@ -79,6 +125,7 @@ function saveDataFromTweet(text, from, place, location, cityName, img) {
       	},
 		"city": cityName
   	});
+	console.log("data saved");
 }
 
 function replyToUser(tweetTo, messageToTweet) {
